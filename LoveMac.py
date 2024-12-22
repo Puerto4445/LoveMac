914e535da190b275e975182616614db6b1387fd2
PUERTO4445 <166276555+Puerto4445@users.noreply.github.com>
2024-12-22 20:14:32 -0300

    LoveMac.py

--- LoveMac.py
+++ LoveMac.py
@@ -22,7 +22,7 @@ def close_correct(sig, frame):
     """CIERRE CONTROLADO"""
     print(
         colored(
-            f"\nTe vas tan rapido amor?...\n",
+            f"\n[!] Goodbye...\n",
             "light_blue",
             "on_light_grey",
             attrs=["dark"],
@@ -42,23 +42,37 @@ def Valid_input(interface, mac):
 
 
 def Changer_MAC(interface, mac):
-    """CAMBIADOR MAC"""
+    """  
+    Cambia la dirección MAC de la interfaz de red proporcionada.  
+
+    Args:  
+        interface (str): La interfaz de red.  
+        mac (str): La nueva dirección MAC.  
+
+    Returns:  
+        None  
+    """  
     if Valid_input(interface, mac):
-        for i in tqdm(range(100), desc="Progreso"):
+        for _ in tqdm(range(100), desc="Progreso"):
             time.sleep(0.1)
         subprocess.run(["ifconfig", interface, "down"])
         subprocess.run(["ifconfig", interface, "hw", "ether", mac])
         subprocess.run(["ifconfig", interface, "up"])
 
         print(
-            colored(f"\n[+] Nueva direccion MAC disponible.", "green", attrs=["bold"])
+            colored(f"\n[+] New MAC available", "green", attrs=["bold"])
         )
     else:
-        print(colored(f"\n[!] No se ha podido camibar la MAC", "red"))
+        print(colored(f"\n[!] MAC not available ", "red"))
 
 
 def arg():
-    """ARGUMENTOS REQUERIDOS"""
+    """  
+    Obtiene los argumentos requeridos para la ejecución del script.  
+
+    Returns:  
+        argparse.Namespace: Los argumentos proporcionados.  
+    """
     parser = argparse.ArgumentParser(description="Cambiar MAC de una intefaz de red")
     parser.add_argument(
         "-i", "--interface", required=True, dest="interface", help="Interfaz de red"
